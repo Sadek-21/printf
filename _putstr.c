6d31e4637da99de0519805bbc0b1b03d7f53cf63
Sadek-21 <mohamed20000327@gmail.com>
2024-02-02 11:26:29 -0800

    updt

--- _putstr.c
+++ _putstr.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/*
+ * _putstr - Prints a string to the standard output.
+ * @s: Pointer to the string to be printed.
+ * @len: Pointer to the length variable for tracking output length.
+ */
+
 void	_putstr(char *s, int *len)
 {
 	if (!s)
